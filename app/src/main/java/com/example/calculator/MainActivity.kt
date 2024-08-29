62ef0721e94de48cbd20e94506d6d8958b768f69
Xera <ddosmukhamedovali@gmail.com>
2024-08-30 00:43:02 +0300

    set onClick for clear button and plus button

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private var canAddOperator= false
+    private var canAddOperator = false
     private var canAddDecimal = true
 
 
@@ -26,56 +26,62 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     fun numberAction(view: View) {
-        var workingsTV : TextView = findViewById(R.id.workings_textview)
-        var resultTV : TextView = findViewById(R.id.result_textview)
+        var workingsTV: TextView = findViewById(R.id.workings_textview)
+        var resultTV: TextView = findViewById(R.id.result_textview)
+
+        // cast view as a button so it is possible to invoke the text attribute
         val button = view as Button
         val buttonText = button.text.toString()
 
-            if (workingsTV.text == "."){
-                if (canAddDecimal) {
-                    workingsTV.append(buttonText)
-                }
-                canAddDecimal = false
-            }
-            else {
+        var zeroCheck: Boolean = workingsTV.text.equals("0")
+
+        if (buttonText == ".") {
+            if (canAddDecimal) { // adds a decimal only if there was no decimal before
                 workingsTV.append(buttonText)
             }
-            canAddOperator = true
+            canAddDecimal = false
+        } else {
+            if (zeroCheck) {
+                workingsTV.text = buttonText
+            } else {
+                workingsTV.append(buttonText)
+            }
+        }
+
+        canAddOperator = true
 
 
     }
 
     fun operatorAction(view: View) {
-        var workingsTV : TextView = findViewById(R.id.workings_textview)
-        var resultTV : TextView = findViewById(R.id.result_textview)
+        var workingsTV: TextView = findViewById(R.id.workings_textview)
+        var resultTV: TextView = findViewById(R.id.result_textview)
         val button = view as Button
         val buttonText = button.text.toString()
 
         if (canAddOperator) {
-            workingsTV.append(buttonText)
+            workingsTV.append(" " + buttonText + " ")
             canAddOperator = false
             canAddDecimal = true
         }
     }
 
     fun allClearAction(view: View) {
-        var workingsTV : TextView = findViewById(R.id.workings_textview)
-        var resultTV : TextView = findViewById(R.id.result_textview)
+        var workingsTV: TextView = findViewById(R.id.workings_textview)
+        var resultTV: TextView = findViewById(R.id.result_textview)
         workingsTV.text = "0"
         resultTV.text = "0"
     }
 
     fun backspaceAction(view: View) {
-        var workingsTV : TextView = findViewById(R.id.workings_textview)
-        var resultTV : TextView = findViewById(R.id.result_textview)
+        var workingsTV: TextView = findViewById(R.id.workings_textview)
+        var resultTV: TextView = findViewById(R.id.result_textview)
         val workingsLength = workingsTV.length()
         if (workingsLength > 1) {
             workingsTV.text = workingsTV.text.subSequence(0, workingsLength - 1)
+        } else {
+            workingsTV.text = "0"
         }
     }
-
-
-
 }
